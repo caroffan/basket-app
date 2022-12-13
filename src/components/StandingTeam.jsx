1594b4605f2aa7e972269baa3144562a78891dbb
Anthony Caroff <anthony.caroff.pro@gmail.com>
2022-12-13 10:07:35 +0100

    Update StandingTeam.jsx

--- src/components/StandingTeam.jsx
+++ src/components/StandingTeam.jsx
@@ -22,72 +22,72 @@ const StandingTeam = ({teams}, {loading}) => {
                 sorter: (a, b) => a.conference.rank - b.conference.rank,
                 sortDirections: ['ascend'],
             },
-        {
-            title: 'Team',
-            dataIndex: 'team',
-            key: 'team.name',
-            render: (record) => {
-                return (<div style={{display: "flex", alignItems: "center"}}>
-                        <img src={record.logo} alt={'icon'} style={{width: 25}}/>
-                        <div>{record.name}</div>
-                    </div>
-                )
-            },
-            align: 'center'
-        },
-        {
-            title: 'W',
-            dataIndex: 'conference',
-            key: 'conference.win',
-            render: (record) => {
-                return (
-                    <div>
-                        <p>{record.win}</p>
-                    </div>
-                )
-            },
-            align: 'center'
-        },
-        {
-            title: 'L',
-            dataIndex: 'conference',
-            key: 'conference.loss',
-            render: (record) => {
-                return (
-                    <div>
-                        <p>{record.loss}</p>
-                    </div>
-                )
-            },
-            align: 'center'
-        },
-        {
-            title: 'W%',
-            dataIndex: 'win',
-            key: 'win.percentage',
-            render: (record) => {
-                return (
-                    <div>
-                        <p>{record.percentage}</p>
-                    </div>
-                )
-            },
-            align: 'center'
-        },
-        {
-            title: 'L10',
-            dataIndex: 'win',
-            key: 'win.lastTen',
-            render: (record) => {
-                return (
-                    <div>
-                        <p>{record.lastTen}-{10 - record.lastTen}</p>
-                    </div>
-                )
-            },
-            align: 'center'
+                {
+                    title: 'Team',
+                    dataIndex: 'team',
+                    key: 'team.name',
+                    render: (record) => {
+                        return (<div style={{display: "flex", alignItems: "center"}}>
+                                <img src={record.logo} alt={'icon'} style={{width: 25}}/>
+                                <div>{record.name}</div>
+                            </div>
+                        )
+                    },
+                    align: 'center'
+                },
+                {
+                    title: 'W',
+                    dataIndex: 'conference',
+                    key: 'conference.win',
+                    render: (record) => {
+                        return (
+                            <div>
+                                <p>{record.win}</p>
+                            </div>
+                        )
+                    },
+                    align: 'center'
+                },
+                {
+                    title: 'L',
+                    dataIndex: 'conference',
+                    key: 'conference.loss',
+                    render: (record) => {
+                        return (
+                            <div>
+                                <p>{record.loss}</p>
+                            </div>
+                        )
+                    },
+                    align: 'center'
+                },
+                {
+                    title: 'W%',
+                    dataIndex: 'win',
+                    key: 'win.percentage',
+                    render: (record) => {
+                        return (
+                            <div>
+                                <p>{record.percentage}</p>
+                            </div>
+                        )
+                    },
+                    align: 'center'
+                },
+                {
+                    title: 'L10',
+                    dataIndex: 'win',
+                    key: 'win.lastTen',
+                    render: (record) => {
+                        return (
+                            <div>
+                                <p>{record.lastTen}-{10 - record.lastTen}</p>
+                            </div>
+                        )
+                    },
+                    align: 'center'
+                }]
         }]
-}    ]
 
     return (
         <Table bordered={true}
